99071d425b5f3fb519e0b5cf68d692f08246a2d1
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-09-15 10:51:55 +0300

    Student class works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Exercise8;
 Console.WriteLine("Hello, World!");
+
+Student objectStudent = new Student("Teppo","Testi");
+Console.WriteLine(objectStudent.Fname+" "+objectStudent.Lname);
